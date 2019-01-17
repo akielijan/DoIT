f4de6c7e815d325fe294cdc24ab9c1fb77d20b46
Natalia Abramowicz <natalia.abramowicz@gmail.com>
2019-01-17 21:58:42 +0100

    wip

---
+++ app/src/main/java/com/potatoprogrammers/doit/SignInActivity.java
@@ -0,0 +1,113 @@
+package com.potatoprogrammers.doit;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.Layout;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A login screen that offers sign in into app via google authentication.
+ */
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideTitleBar();
+        setContentView(R.layout.activity_sign_in);
+
+        Button signInButton = findViewById(R.id.sign_in_action_button);
+        TextView signInErrorArea = findViewById(R.id.sign_in_error);
+
+        signInErrorArea.setText(""); //todo current error state
+
+        signInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptSignInWithGoogle();
+            }
+        });
+    }
+
+    private void hideTitleBar() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    /**
+     * Attempts to sign in using google authentication.
+     * todo implement google auth
+     */
+    private void attemptSignInWithGoogle() {
+        showProgress(true);
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        LinearLayout signInContent = findViewById(R.id.sign_in_layout);
+        ProgressBar progressBar = findViewById(R.id.login_progress);
+
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            signInContent.setVisibility(show ? View.GONE : View.VISIBLE);
+            signInContent.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    signInContent.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            progressBar.setVisibility(show ? View.VISIBLE : View.GONE);
+            progressBar.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    progressBar.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            progressBar.setVisibility(show ? View.VISIBLE : View.GONE);
+            signInContent.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+}
+
