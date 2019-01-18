a222ac7a11eb933b1509e9a057c4d82210a94fbf
Adrian Kielijan <akielijan@interia.pl>
2019-01-18 21:16:18 +0100

    added google sign in

--- app/src/main/java/com/potatoprogrammers/doit/SignInActivity.java
+++ app/src/main/java/com/potatoprogrammers/doit/SignInActivity.java
@@ -18,6 +18,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.Layout;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -28,14 +29,36 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * A login screen that offers sign in into app via google authentication.
  */
 public class SignInActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 1;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            //todo: start activity
+            Toast.makeText(getApplicationContext(), "Potato", Toast.LENGTH_SHORT).show();
+        }
+        updateUI(account);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +71,13 @@ public class SignInActivity extends AppCompatActivity {
 
         signInErrorArea.setText(""); //todo current error state
 
-        signInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptSignInWithGoogle();
-            }
-        });
+        signInButton.setOnClickListener(view -> attemptSignInWithGoogle());
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
     private void hideTitleBar() {
@@ -68,8 +92,52 @@ public class SignInActivity extends AppCompatActivity {
      * todo implement google auth
      */
     private void attemptSignInWithGoogle() {
-        showProgress(true);
-        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        //showProgress(true);
+        //startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Toast.makeText(getApplicationContext(), "onActivityResult", Toast.LENGTH_SHORT).show();
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            Toast.makeText(getApplicationContext(), "handleSingInResult", Toast.LENGTH_SHORT).show();
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            //Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(getApplicationContext(), String.format(Locale.getDefault(), "Sing in failed: error: %d", e.getStatusCode()), Toast.LENGTH_SHORT).show();
+            updateUI(null);
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        Toast.makeText(getApplicationContext(), "updateUI", Toast.LENGTH_SHORT).show();
+        if (account != null) {
+            Toast.makeText(getApplicationContext(), account.getDisplayName(), Toast.LENGTH_SHORT).show();
+            showProgress(true);
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        } else {
+            Toast.makeText(getApplicationContext(), "Account is null", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
