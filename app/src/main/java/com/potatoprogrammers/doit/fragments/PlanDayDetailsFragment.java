29fdf562df4373d4b50fea9f2bad59b0a25f1ca9
Natalia Abramowicz <natalia.abramowicz@gmail.com>
2019-01-24 04:46:09 +0100

    day wip

--- app/src/main/java/com/potatoprogrammers/doit/fragments/PlanDayDetailsFragment.java
+++ app/src/main/java/com/potatoprogrammers/doit/fragments/PlanDayDetailsFragment.java
@@ -2,6 +2,8 @@ package com.potatoprogrammers.doit.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,22 +11,28 @@ import android.view.ViewGroup;
 
 import com.potatoprogrammers.doit.R;
 
+import java.util.Date;
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PlanDayDetailsFragment extends Fragment {
-
+public class PlanDayDetailsFragment extends AbstractFragment {
+    private Date date;
 
     public PlanDayDetailsFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_plan_day_details, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        date = (Date) getArguments().getSerializable("date");
+    }
 }
